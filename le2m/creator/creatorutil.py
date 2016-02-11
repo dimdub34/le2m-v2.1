d6baf98e4b8eeca588ee88734be11a3c81b96dd9
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2016-02-11 10:44:23 +0100

    Use of a temp directory for creator.log

--- le2m/creator/creatorutil.py
+++ le2m/creator/creatorutil.py
@@ -4,7 +4,8 @@ __author__ = "Dimitri DUBOIS"
 import os
 import gettext
 import logging
-from logging.handlers import RotatingFileHandler
+import tempfile
+# from logging.handlers import RotatingFileHandler
 
 _current_directory = os.path.dirname(os.path.realpath(__file__))
 _localedir = os.path.join(_current_directory, "locale")
@@ -21,7 +22,9 @@ def create_logger():
     formatter = logging.Formatter(
         '%(name)s | %(asctime)s | %(levelname)s | %(message)s',
         datefmt="%d/%m/%Y - %H:%M:%S")
-    file_handler = RotatingFileHandler('creator.log', 'a', 1000000, 1)
+    # file_handler = RotatingFileHandler('creator.log', 'a', 1000000, 1)
+    file_handler = logging.FileHandler(
+        os.path.join(tempfile.gettempdir(), "creator.log"))
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
