ef46c5d18ba8962cccce84bc23d324614c0dceb0
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2017-01-05 16:18:46 +0100

    Change the implementation of install in order to have le2mtrans even if we don't run le2m_client

--- le2m/util/utili18n.py
+++ le2m/util/utili18n.py
@@ -6,15 +6,18 @@ import gettext
 
 import configuration.configparam as params
 
-localedir = os.path.join(params.getp("APPDIR"), "locale")
+# localedir = os.path.join(params.getp("APPDIR"), "locale")
 le2mtrans = None
 
 
 def install():
     global le2mtrans
     try:
+        localedir = os.path.join(params.getp("APPDIR"), "locale")
         le2mtrans = gettext.translation(
             "le2m", localedir, languages=[params.getp("LANG")]).ugettext
+    except AttributeError:
+        le2mtrans = lambda x: x
     except IOError:
         le2mtrans = gettext.translation("le2m", localedir).ugettext
 
