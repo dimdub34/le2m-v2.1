c1226f57a708e32f95539378087b2abdfb720bcd
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2018-02-18 23:14:56 +0100

    add RepeatedTimer, a class that repeats a function regularly depending on an interval parameter

--- le2m/util/utiltools.py
+++ le2m/util/utiltools.py
@@ -11,8 +11,38 @@ import inspect
 from PyQt4.QtCore import QObject, QTimer, pyqtSignal  # pour CompteARebours
 from random import randint
 import numpy as np
+import threading
+import time
 
 logger = logging.getLogger("le2m")
+
+
+class RepeatedTimer(object):
+    def __init__(self, interval, func, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.next_call = time.time()
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.func(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self.next_call += self.interval
+            self._timer = threading.Timer(self.next_call - time.time(), self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 
 def get_formatedtimefromseconds(nombre_secondes):
