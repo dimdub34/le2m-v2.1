f957dfda138f35ccf9fe53479d690976b90d2e9e
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2018-03-02 23:05:21 +0100

    work in progress

--- le2m/server/serv.py
+++ le2m/server/serv.py
@@ -1,17 +1,22 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-Ce module est le modèle du serveur. Il crée une instance de chacun des
-gestionnaires (graphique, joueurs, groupes, expérience ...)
-"""
+""" ============================================================================
 
+This modules only contains the server. It creates an instance of the various
+models (experiment, groups, base and so on).
+
+============================================================================ """
+
+# built-in
 import os
 import socket
 import logging
 from time import strftime
 from twisted.internet import defer
 from twisted.spread import pb
+
+# le2m
 from configuration import configparam as params
 from configuration.configvar import Experiment
 from servgest.servgestgui import GestionnaireGraphique
@@ -19,6 +24,7 @@ from servgest.servgestexpe import GestionnaireExperience
 from servgest.servgestbase import GestionnaireBase
 from servgest.servgestplayers import GestionnaireJoueurs
 from servgest.servgestgroups import GestionnaireGroupes
+
 # twisted reactor is imported after gestionnaire_graphique because PyQt
 # install a common reactor for PyQt and twisted
 from twisted.internet import reactor
@@ -33,30 +39,43 @@ class Serveur(object):
     """
     def __init__(self, **kwargs):
 
+        # ----------------------------------------------------------------------
         # network infos
-        self._port_ecoute = kwargs["port"]
-        self._hostname = socket.gethostname()
-        self._ip = socket.gethostbyname(self._hostname)
-        logger.info("Hostname: {}".format(self._hostname))
-        logger.info("IP: {}".format(self._ip))
+        # ----------------------------------------------------------------------
+        self.port_ecoute = kwargs["port"]
+        self.hostname = socket.gethostname()
+        self.ip = socket.gethostbyname(self.hostname)
+
+        logger.info("Hostname: {}".format(self.hostname))
+        logger.info("IP: {}".format(self.ip))
         
-        # création de la session
-        self._heure_debut = strftime("%H:%M:%S")
-        self._nom_session = strftime('%Y%m%d%H%M')
-        logger.info("Session name: {}".format(self._nom_session))
-        
-        # les gestionnaires
-        self.gestionnaire_graphique = GestionnaireGraphique(self)
+        # ----------------------------------------------------------------------
+        # create the session
+        # ----------------------------------------------------------------------
+        self.heure_debut = strftime("%H:%M:%S")
+        self.nom_session = strftime('%Y%m%d%H%M')
+        logger.info("Session name: {}".format(self.nom_session))
+
+        # ----------------------------------------------------------------------
+        # Instantiate the models
+        # ----------------------------------------------------------------------
         self.gestionnaire_base = GestionnaireBase(self)
         self.gestionnaire_joueurs = GestionnaireJoueurs(self)
         self.gestionnaire_groupes = GestionnaireGroupes(self)
         self.gestionnaire_experience = GestionnaireExperience(self)
+        self.gestionnaire_graphique = GestionnaireGraphique(self)
 
-        # we connect some slot now, after instanciating the other gestionnaires
-        self.gestionnaire_graphique.screen.connect_slots()
-        self.gestionnaire_graphique.screen.show()
-        
-        # if option -e in the command line
+        # ----------------------------------------------------------------------
+        # we connect some slot now, after the instantiation of the other
+        # gestionnaires
+        # ----------------------------------------------------------------------
+        self.screen = self.gestionnaire_graphique.screen  # just a shortcut
+        # self.screen.connect_slots()
+        self.screen.show()
+
+        # ----------------------------------------------------------------------
+        # load a part if it is in the command line options (arg -e)
+        # ----------------------------------------------------------------------
         if kwargs["parts"]:
             expeinfos = Experiment(
                 kwargs["parts"],
@@ -64,30 +83,6 @@ class Serveur(object):
                     params.getp("PARTSDIR"), kwargs["parts"][0]),
                 kwargs["namebase"], kwargs["test"])
             self.gestionnaire_experience.load_experiment(expeinfos)
-
-    # --------------------------------------------------------------------------
-    # PROPERTIES
-    # --------------------------------------------------------------------------
-
-    @property
-    def heure_debut(self):
-        return self._heure_debut
-
-    @property
-    def nom_session(self):
-        return self._nom_session
-
-    @property
-    def hostname(self):
-        return self._hostname
-
-    @property
-    def ip(self):
-        return self._ip
-
-    @property
-    def port_ecoute(self):
-        return self._port_ecoute
 
     # --------------------------------------------------------------------------
     # METHODS
@@ -98,8 +93,8 @@ class Serveur(object):
         Start the server.
         """
         factory = pb.PBServerFactory(self.gestionnaire_joueurs)
-        reactor.listenTCP(self._port_ecoute, factory)
-        logger.info(u"Server is listening on port {}".format(self._port_ecoute))
+        reactor.listenTCP(self.port_ecoute, factory)
+        logger.info(u"Server is listening on port {}".format(self.port_ecoute))
         # boucle réseau et graphique
         reactor.run()
   
