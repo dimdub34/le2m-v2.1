b9c64942254265dd61ca9734c6c49f3e2bd8e2ae
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2018-03-12 09:34:24 +0100

    a simple reorganization of the code

--- le2m/server/servgest/servgestexpe.py
+++ le2m/server/servgest/servgestexpe.py
@@ -26,11 +26,15 @@ class GestionnaireExperience(QObject):
 
     def __init__(self, le2msrv):
         super(GestionnaireExperience, self).__init__()
-        self._le2msrv = le2msrv
+        self.le2msrv = le2msrv
         self._parts = []  # when a part is initiated it is added in this list
         self._servs = {}  # keep a ref to the loaded partserv
         self._stop_repetitions = False  # to stop the part at the next period
         self._screenpayoffs = None
+
+    # ==========================================================================
+    # Load and init
+    # ==========================================================================
 
     def load_experiment(self, expeinfos):
         """
@@ -44,8 +48,8 @@ class GestionnaireExperience(QObject):
             try:
                 module = importlib.import_module("parts.{0}.{0}Serv".format(p))
                 serv = getattr(module, "Serveur")
-                self._servs[p] = serv(self._le2msrv)
-                self._le2msrv.gestionnaire_graphique.infoserv(
+                self._servs[p] = serv(self.le2msrv)
+                self.le2msrv.gestionnaire_graphique.infoserv(
                     le2mtrans(u"Part") + " {} ".format(p) + le2mtrans(u"loaded"))
             except ImportError as e:
                 logger.critical(
@@ -53,8 +57,8 @@ class GestionnaireExperience(QObject):
                         p=p, msg=e.message))
 
         # database creation
-        if not self._le2msrv.gestionnaire_base.is_created():
-            self._le2msrv.gestionnaire_base.creer_base(
+        if not self.le2msrv.gestionnaire_base.is_created():
+            self.le2msrv.gestionnaire_base.creer_base(
                 expeinfos.dirbase, expeinfos.basename, expeinfos.istest)
 
     @defer.inlineCallbacks
@@ -71,17 +75,17 @@ class GestionnaireExperience(QObject):
         :param partparameters: parameters of the part
         :return:
         """
-        self._le2msrv.gestionnaire_graphique.infoserv(None)
-        self._le2msrv.gestionnaire_graphique.infoserv(
+        self.le2msrv.gestionnaire_graphique.infoserv(None)
+        self.le2msrv.gestionnaire_graphique.infoserv(
             partname.upper(), fg="white", bg="blue")
-        self._le2msrv.gestionnaire_graphique.infoclt(None)
-        self._le2msrv.gestionnaire_graphique.infoclt(
+        self.le2msrv.gestionnaire_graphique.infoclt(None)
+        self.le2msrv.gestionnaire_graphique.infoclt(
             partname.upper(), fg="white", bg="blue")
 
         # the players instantiate the part and get the corresponding remote
-        players = self._le2msrv.gestionnaire_joueurs.get_players()
-        self._le2msrv.gestionnaire_graphique.set_waitmode(players)
-        yield (self.run_func(players, "add_part", self._le2msrv,
+        players = self.le2msrv.gestionnaire_joueurs.get_players()
+        self.le2msrv.gestionnaire_graphique.set_waitmode(players)
+        yield (self.run_func(players, "add_part", self.le2msrv,
                              partname, partclassname, remoteclassname, **kwargs))
 
         self._parts.append(partname)
@@ -90,26 +94,126 @@ class GestionnaireExperience(QObject):
         # we store the parameters in the database (in table session)
         paramcontent = utiltools.get_module_info(partparameters)
         sesscontent = \
-            self._le2msrv.gestionnaire_base.get_session().parametres or u""
+            self.le2msrv.gestionnaire_base.get_session().parametres or u""
         sesscontent += u"{}PART {}\n{}".format(
             u"\n\n" if sesscontent else u"", partname, paramcontent)
         logger.debug(sesscontent)
-        self._le2msrv.gestionnaire_base.get_session().parametres = sesscontent
-        self._le2msrv.gestionnaire_base.enregistrer()
+        self.le2msrv.gestionnaire_base.get_session().parametres = sesscontent
+        self.le2msrv.gestionnaire_base.enregistrer()
 
-        self._le2msrv.gestionnaire_graphique.infoserv(
+        self.le2msrv.gestionnaire_graphique.infoserv(
             le2mtrans(u"Start time: {st}").format(st=strftime("%H:%M:%S")))
+
+    # ==========================================================================
+    # Generic functions
+    # ==========================================================================
+
+    @defer.inlineCallbacks
+    def run_step(self, step_name, step_participants, step_function, *args,
+                 **kwargs):
+        """
+        Run the step:
+        - display the name of the step on the client list
+        - run the function with the parameters on the given participants
+        - display the end of the step on the server list
+        """
+        logger.info(u"run_step: {}".format(step_name))
+        self.le2msrv.gestionnaire_graphique.infoclt(step_name.upper(), fg="gray")
+        self.le2msrv.gestionnaire_graphique.set_waitmode(step_participants)
+        yield (utiltwisted.forAll(
+            step_participants, step_function, *args, **kwargs))
+        self.le2msrv.gestionnaire_graphique.infoserv(
+            u"Ok {}".format(step_name), fg="green")
+        self.le2msrv.gestionnaire_base.enregistrer()
+
+    @defer.inlineCallbacks
+    def run_func(self, who, func, *args, **kwargs):
+        """
+        Run the func with the given parameters on the given participants
+        """
+        logger.info(u"run_func: {}".format(func))
+        if not type(who) is list:
+            raise ValueError(le2mtrans(u"The arg must be of type list"))
+        yield (utiltwisted.forAll(who, func, *args, **kwargs))
+
+    # ==========================================================================
+    # functions that only affect the server
+    # ==========================================================================
+
+    def draw_part(self, parts):
+        """
+        Draw randomly a part among thoses played
+        """
+        drawnpart = random.choice(parts)
+        partnum = self._parts.index(drawnpart) + 1
+        logger.info(u"Part randomly drawn: {}".format(drawnpart))
+        self.le2msrv.gestionnaire_graphique.infoserv(
+            le2mtrans(u"Part randomly drawn: {} (part {})").format(
+                drawnpart, partnum))
+
+    def display_payoffs_onserver(self, partname):
+        """
+        Open a dialog box with the payoffs
+        """
+        payoffs = []
+        try:
+            joueurs = sorted(self.le2msrv.gestionnaire_joueurs.get_players(
+                partname), key=lambda x: x.joueur.hostname)
+        except TypeError:  # no client connected
+            QtGui.QMessageBox.warning(
+                self.le2msrv.gestionnaire_graphique.screen,
+                le2mtrans(u"warning"), le2mtrans(u"No client connected"))
+            return
+        try:
+            if partname == "base":
+                for j in joueurs:
+                    payoffs.append(
+                        [j.joueur.hostname, "{:.2f}".format(j.paiementFinal)])
+            else:
+                for j in joueurs:
+                    gain_partie = getattr(j, "{}_gain_euros".format(
+                        j.nom_court))
+                    payoffs.append(
+                        [j.joueur.hostname, "{:.2f}".format(gain_partie)])
+
+        except (AttributeError, KeyError) as e:
+            QtGui.QMessageBox.critical(
+                self.le2msrv.gestionnaire_graphique.screen,
+                le2mtrans(u"Error"),
+                le2mtrans(u"Error while getting payoffs for "
+                          u"part {}: {}").format(partname, e.message))
+            return
+
+        self._screenpayoffs = GuiPayoffs(self.le2msrv, partname, payoffs)
+        self._screenpayoffs.show()
+
+    def add_tofinalpayoffs(self, partname):
+        """
+        Add the part payoffs to the final payoffs
+        """
+        for p in self.le2msrv.gestionnaire_joueurs.get_players(partname):
+            gain_euros = getattr(p, "{}_gain_euros".format(
+                    p.nom_court))
+            p.joueur.get_part('base').paiementFinal += gain_euros
+        self.le2msrv.gestionnaire_base.enregistrer()
+        self.le2msrv.gestionnaire_graphique.infoserv(
+            le2mtrans(u"Payoffs of part {} added to final payoffs").format(
+                partname), fg="red")
+
+    # ==========================================================================
+    # Functions that call the players' remote
+    # ==========================================================================
 
     @defer.inlineCallbacks
     def display_welcome(self):
         """
         Display the welcome screen on remotes
         """
-        participants = self._le2msrv.gestionnaire_joueurs.get_players("base")
+        participants = self.le2msrv.gestionnaire_joueurs.get_players("base")
         if not participants:
             return
 
-        self._le2msrv.gestionnaire_graphique.infoclt(None)
+        self.le2msrv.gestionnaire_graphique.infoclt(None)
         yield (self.run_step(u"Instructions", participants, "display_welcome"))
 
     @defer.inlineCallbacks
@@ -117,7 +221,7 @@ class GestionnaireExperience(QObject):
         """
         Start the understanding questionnaire.
         """
-        participants = self._le2msrv.gestionnaire_joueurs. \
+        participants = self.le2msrv.gestionnaire_joueurs. \
             get_players("base")
         if not participants:
             return
@@ -135,21 +239,20 @@ class GestionnaireExperience(QObject):
         computed
         """
         if not self.has_part(partname):
-            raise ValueError(le2mtrans(u"Part {p} not in the list").format(
-                p=partname))
+            raise ValueError(le2mtrans(
+                u"Part {} not in the list").format(partname))
 
-        logger.info(
-            le2mtrans(u"Computation of period payoffs for part {p}").format(
-                p=partname))
-        self._le2msrv.gestionnaire_graphique.\
+        logger.info(le2mtrans(
+            u"Computation of period payoffs for part {}").format(partname))
+        self.le2msrv.gestionnaire_graphique.\
             infoclt(le2mtrans(u"PAYOFFS"), fg="gray")
-        for p in self._le2msrv.gestionnaire_joueurs.get_players(partname):
+        for p in self.le2msrv.gestionnaire_joueurs.get_players(partname):
             p.compute_periodpayoff()
             payoff = getattr(p.currentperiod,
                              "{}_periodpayoff".format(p.nom_court))
-            self._le2msrv.gestionnaire_graphique.infoclt(
+            self.le2msrv.gestionnaire_graphique.infoclt(
                 u"{}: {}".format(p.joueur, payoff))
-        self._le2msrv.gestionnaire_base.enregistrer()
+        self.le2msrv.gestionnaire_base.enregistrer()
 
     @defer.inlineCallbacks
     def finalize_part(self, partname, *args, **kwargs):
@@ -165,25 +268,25 @@ class GestionnaireExperience(QObject):
         logger.info(
             le2mtrans(u"Computation of payoffs for part {p}").format(
                 p=partname))
-        self._le2msrv.gestionnaire_graphique.infoclt(
+        self.le2msrv.gestionnaire_graphique.infoclt(
             [None, le2mtrans(u'Payoffs of part {p}').format(
                 p=partname.upper())], fg="red")
 
-        players = self._le2msrv.gestionnaire_joueurs.get_players(partname)
+        players = self.le2msrv.gestionnaire_joueurs.get_players(partname)
         yield (self.run_func(players, "compute_partpayoff", *args, **kwargs))
 
         for p in players:
             payoff = getattr(p, "{}_gain_euros".format(p.nom_court))
-            self._le2msrv.gestionnaire_graphique.infoclt(
+            self.le2msrv.gestionnaire_graphique.infoclt(
                 u"{}: {}".format(p.joueur, payoff))
-        self._le2msrv.gestionnaire_base.enregistrer()
+        self.le2msrv.gestionnaire_base.enregistrer()
 
         # Finalization of part
-        self._le2msrv.gestionnaire_graphique.infoserv(
+        self.le2msrv.gestionnaire_graphique.infoserv(
             le2mtrans(u"End time: {et}").format(et=strftime("%H:%M:%S")))
-        self._le2msrv.gestionnaire_graphique.infoclt(
+        self.le2msrv.gestionnaire_graphique.infoclt(
             u'Ok {}'.format(partname).upper(), fg="white", bg="blue")
-        self._le2msrv.gestionnaire_graphique.infoserv(
+        self.le2msrv.gestionnaire_graphique.infoserv(
             u'Ok {}'.format(partname).upper(), fg="white", bg="blue")
 
     @defer.inlineCallbacks
@@ -191,52 +294,29 @@ class GestionnaireExperience(QObject):
         """
         Start the final questionnaire
         """
-        participants = self._le2msrv.gestionnaire_joueurs. \
+        participants = self.le2msrv.gestionnaire_joueurs. \
             get_players('questionnaireFinal')
         if not participants:
             return
-        self._le2msrv.gestionnaire_graphique.infoclt(None)
+        self.le2msrv.gestionnaire_graphique.infoclt(None)
         yield (self.run_step(
             u"Questionnaire final", participants, "start"))
 
-    def draw_part(self, parts):
-        """
-        Draw randomly a part among thoses played
-        """
-        drawnpart = random.choice(parts)
-        partnum = self._parts.index(drawnpart) + 1
-        logger.info(u"Part randomly drawn: {}".format(drawnpart))
-        self._le2msrv.gestionnaire_graphique.infoserv(
-            le2mtrans(u"Part randomly drawn: {} (part {})").format(
-                drawnpart, partnum))
-
     @defer.inlineCallbacks
     def display_finalscreen(self):
-        self._le2msrv.gestionnaire_graphique.infoclt(None)
+        self.le2msrv.gestionnaire_graphique.infoclt(None)
         yield (self.run_step(
             le2mtrans(u"Final screen"),
-            self._le2msrv.gestionnaire_joueurs.get_players("base"),
+            self.le2msrv.gestionnaire_joueurs.get_players("base"),
             "display_finalscreen"))
-
-    # @defer.inlineCallbacks
-    # def display_images(self, directory):
-    #     images = [i for i in os.listdir(directory) if i.endswith(".jpg") or
-    #               i.endswith(".png")]
-    #     logger.debug("Images: {}".format(images))
-    #     self._le2msrv.gestionnaire_graphique.display_images(directory, images)
-    #     with open(os.path.join(directory, images[0])) as im:
-    #         image = im.read()
-    #     yield (utiltwisted.forAll(
-    #         self._le2msrv.gestionnaire_joueurs.get_players("base"),
-    #         "display_image", image))
 
     @defer.inlineCallbacks
     def display_payoffs_onremotes(self, part_or_listOfParts):
         """
         Display the final text of each parts on the remote
         """
-        self._le2msrv.gestionnaire_graphique.infoclt(None)
-        players = self._le2msrv.gestionnaire_joueurs.get_players("base")
+        self.le2msrv.gestionnaire_graphique.infoclt(None)
+        players = self.le2msrv.gestionnaire_joueurs.get_players("base")
         if type(part_or_listOfParts) is str:
             yield (self.run_step(
                 le2mtrans(u"Display part payoffs"), players, "display_payoffs",
@@ -249,104 +329,27 @@ class GestionnaireExperience(QObject):
             step = le2mtrans(u"Display details of payoffs for parts") + \
                     u" {}".format(u", ".join(
                         [v for k, v in sorted(orderedparts.viewitems())]))
-            self._le2msrv.gestionnaire_graphique.infoclt(None)
+            self.le2msrv.gestionnaire_graphique.infoclt(None)
             yield (self.run_step(
                 step, players, "display_payoffs", orderedparts))
 
-    def display_payoffs_onserver(self, partname):
-        """
-        Open a dialog box with the payoffs
-        """
-        payoffs = []
-        try:
-            joueurs = sorted(self._le2msrv.gestionnaire_joueurs.get_players(
-                partname), key=lambda x: x.joueur.hostname)
-        except TypeError:  # no client connected
-            QtGui.QMessageBox.warning(
-                self._le2msrv.gestionnaire_graphique.screen,
-                le2mtrans(u"warning"), le2mtrans(u"No client connected"))
-            return
-        try:
-            if partname == "base":
-                for j in joueurs:
-                    payoffs.append(
-                        [j.joueur.hostname, "{:.2f}".format(j.paiementFinal)])
-            else:
-                for j in joueurs:
-                    gain_partie = getattr(j, "{}_gain_euros".format(
-                        j.nom_court))
-                    payoffs.append(
-                        [j.joueur.hostname, "{:.2f}".format(gain_partie)])
-
-        except (AttributeError, KeyError) as e:
-            QtGui.QMessageBox.critical(
-                self._le2msrv.gestionnaire_graphique.screen,
-                le2mtrans(u"Error"),
-                le2mtrans(u"Error while getting payoffs for "
-                          u"part {}: {}").format(partname, e.message))
-            return
-
-        self._screenpayoffs = GuiPayoffs(self._le2msrv, partname, payoffs)
-        self._screenpayoffs.show()
-
-    def add_tofinalpayoffs(self, partname):
-        """
-        Ajoute les gains de la partie aux gains finaux
-        """
-        for p in self._le2msrv.gestionnaire_joueurs.get_players(partname):
-            gain_euros = getattr(p, "{}_gain_euros".format(
-                    p.nom_court))
-            p.joueur.get_part('base').paiementFinal += gain_euros
-        self._le2msrv.gestionnaire_base.enregistrer()
-        self._le2msrv.gestionnaire_graphique.infoserv(
-            le2mtrans(u"Payoffs of part {} added to final payoffs").format(
-                partname), fg="red")
-
-    @defer.inlineCallbacks
-    def run_step(self, step_name, step_participants, step_function, *args,
-                 **kwargs):
-        """
-        Lance l'étape donnée, cad:
-        - affiche le nom de l'étape sur la liste client
-        - lance la fonction step_function avec les paramètres donnés
-        chez les participants concernés
-        - affiche la fin d'étape sur la liste serveur
-        """
-        logger.info(u"run_step: {}".format(step_name))
-        self._le2msrv.gestionnaire_graphique. \
-            infoclt(step_name.upper(), fg="gray")
-        self._le2msrv.gestionnaire_graphique.set_waitmode(
-            step_participants)
-        yield (utiltwisted.forAll(
-            step_participants, step_function, *args, **kwargs))
-        self._le2msrv.gestionnaire_graphique. \
-            infoserv(u"Ok {}".format(step_name), fg="green")
-        self._le2msrv.gestionnaire_base.enregistrer()
-
-    @defer.inlineCallbacks
-    def run_func(self, qui, func, *args, **kwargs):
-        """
-        Lance la fonction func avec les paramètres données chez les
-        participants concernés
-        """
-        logger.info(u"run_func: {}".format(func))
-        if not type(qui) is list:
-            raise ValueError(le2mtrans(u"The arg must be of type list"))
-        yield (utiltwisted.forAll(qui, func, *args, **kwargs))
+    # ==========================================================================
+    # properties, setters and getters
+    # ==========================================================================
 
     @property
     def stop_repetitions(self):
         return self._stop_repetitions
 
     @stop_repetitions.setter
-    def stop_repetitions(self, vrai_ou_faux):
+    def stop_repetitions(self, yes_or_no):
         """
         Stop (or cancel) the part at the next period. Emit a signal in order
         the le2m server screen to update (menu options)
-        :param vrai_ou_faux:
+        :param yes_or_no:
         :return:
         """
-        self._stop_repetitions = vrai_ou_faux
+        self._stop_repetitions = yes_or_no
         if self._stop_repetitions:
             logger.info(le2mtrans(u"The game will stop next period"))
         else:
