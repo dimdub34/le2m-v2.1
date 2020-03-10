58e12b085a7b199d9ebc4dd5959d9336181307e6
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2020-03-10 09:05:55 +0100

    ajout table model pour pandas dataframe

--- le2m/server/servgui/servguitablemodels.py
+++ le2m/server/servgui/servguitablemodels.py
@@ -366,3 +366,27 @@ class TableModelPaiements(QtCore.QAbstractTableModel):
         self.lignes.append(ligne)
         self.endInsertRows()
         return
+
+
+class TableModelPandasDataFrame(QtCore.QAbstractTableModel):
+
+    def __init__(self, data):
+        QtCore.QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if index.isValid():
+            if role == QtCore.Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
